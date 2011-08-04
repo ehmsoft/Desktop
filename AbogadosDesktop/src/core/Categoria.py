a25d795b00776e2998dbff1b4a9c16668ad46d0e
Andrés Torres <elfotografo007@gmail.com>
2011-08-03 22:47:48 -0500

    Se crea la clase Categoria

---
+++ AbogadosDesktop/src/core/Categoria.py
@@ -0,0 +1,25 @@
+'''
+Created on 03/08/2011
+
+@author: elfotografo007
+'''
+
+class Categoria:
+    '''
+    Clase Categoria
+    '''
+
+
+    def __init__(self, descripcion, id_categoria = None):
+        self.__id_categoria = id_categoria
+        self.__descripcion = descripcion
+    def getId_categoria(self):
+        return self.__id_categoria
+    def getDescripcion(self):
+        return self.__descripcion
+    def setId_categoria(self, id_categoria):
+        self.__id_categoria = id_categoria
+    def setDescripcion(self, descripcion):
+        self.__descripcion = descripcion
+    def __str__(self):
+        return self.getDescripcion()
