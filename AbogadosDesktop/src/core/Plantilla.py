19d0f3f7a40d4384698614a25781a4b3268760e2
Andrés Torres <elfotografo007@gmail.com>
2011-08-05 10:41:03 -0500

    Se implementa la clase Plantilla

--- AbogadosDesktop/src/core/Plantilla.py
+++ AbogadosDesktop/src/core/Plantilla.py
@@ -1,5 +1,5 @@
 '''
-Created on 04/08/2011
+Created on 05/08/2011
 
 @author: elfotografo007
 '''
@@ -10,50 +10,44 @@ from Persona import Persona
 from CampoPersonalizado import CampoPersonalizado
 
 
-class Proceso(object):
+class Plantilla(object):
     '''
-    Clase Proceso
+    Clase Plantilla
     '''
 
 
-    def __init__(self, demandante, demandado, fecha, juzgado, radicado, radicadoUnico, actuaciones, estado, categoria, tipo, notas, campos, prioridad, id_proceso = None):
+    def __init__(self, nombre, demandante, demandado, juzgado, radicado, radicadoUnico, estado, categoria, tipo, notas, campos, prioridad, id_plantilla = None):
+        self.__nombre = nombre
         self.__demandante = demandante
         self.__demandado = demandado
-        self.__fecha = fecha
         self.__juzgado = juzgado
         self.__radicado = radicado
         self.__radicadoUnico = radicadoUnico
-        self.__actuaciones = actuaciones
         self.__estado = estado
         self.__categoria = categoria
         self.__tipo = tipo
-        self.__notas = notas
+        self.__notas  = notas
         self.__campos = campos
         self.__prioridad = prioridad
-        self.__id_proceso = id_proceso
-    def addActuacion(self, actuacion):
-        self.__actuaciones.append(actuacion)
-    def delActuacion(self, actuacion):
-        self.__actuaciones.remove(actuacion)
+        self.__id_plantilla = id_plantilla
+     
     def addCampo(self, campo):
         self.__campos.append(campo)
     def delCampo(self, campo):
         self.__campos.remove(campo)
     #Getters
+    def getNombre(self):
+        return self.__nombre
     def getDemandante(self):
         return self.__demandante
     def getDemandado(self):
         return self.__demandado
-    def getFecha(self):
-        return self.__fecha
     def getJuzgado(self):
         return self.__juzgado
     def getRadicado(self):
         return self.__radicado
     def getRadicadoUnico(self):
         return self.__radicadoUnico
-    def getActuaciones(self):
-        return self.__actuaciones
     def getEstado(self):
         return self.__estado
     def getCategoria(self):
@@ -66,23 +60,22 @@ class Proceso(object):
         return self.__campos
     def getPrioridad(self):
         return self.__prioridad
-    def getId_proceso(self):
-        return self.__id_proceso
+    def getId_plantilla(self):
+        return self.__id_plantilla
+    
     #Setters
+    def setNombre(self, nombre):
+        self.__nombre = nombre
     def setDemandante(self, demandante):
         self.__demandante = demandante
-    def setDemandado(self, demandado):
+    def setDemandado(self,demandado):
         self.__demandado = demandado
-    def setFecha(self, fecha):
-        self.__fecha = fecha
     def setJuzgado(self, juzgado):
         self.__juzgado = juzgado
     def setRadicado(self, radicado):
         self.__radicado = radicado
     def setRadicadoUnico(self, radicadoUnico):
         self.__radicadoUnico = radicadoUnico
-    def setActuaciones(self, actuaciones):
-        self.__actuaciones = actuaciones
     def setEstado(self, estado):
         self.__estado = estado
     def setCategoria(self, categoria):
@@ -95,5 +88,5 @@ class Proceso(object):
         self.__campos = campos
     def setPrioridad(self, prioridad):
         self.__prioridad = prioridad
-    def setId_proceso(self, id_proceso):
-        self.__id_proceso = id_proceso
+    def setId_plantilla(self, id_plantilla):
+        self.__id_plantilla = id_plantilla
