3e2b3261e8eaffad61a6adcca4c66cbf99a9cb72
Andrés Torres <elfotografo007@gmail.com>
2011-08-05 15:42:04 -0500

    Se hace verificación de tipos en Juzgado

--- AbogadosDesktop/src/core/Juzgado.py
+++ AbogadosDesktop/src/core/Juzgado.py
@@ -3,7 +3,7 @@ Created on 04/08/2011
 
 @author: elfotografo007
 '''
-
+from types import NoneType
 class Juzgado(object):
     '''
     Clase Juzgado
@@ -11,13 +11,36 @@ class Juzgado(object):
 
 
     def __init__(self, nombre = None, ciudad = None, direccion = None, telefono = None, tipo = None, id_juzgado = None):
-        self.__nombre = nombre
-        self.__ciudad = ciudad
-        self.__direccion = direccion
-        self.__telefono = telefono
-        self.__tipo = tipo
-        self.__id_juzgado = id_juzgado
-
+        if isinstance(nombre, basestring) or isinstance(nombre, NoneType):
+            self.__nombre = nombre
+        else:
+            raise TypeError('Tipo de dato no admitido')
+        
+        if isinstance(ciudad, basestring) or isinstance(ciudad, NoneType):
+            self.__ciudad = ciudad
+        else:
+            raise TypeError('Tipo de dato no admitido')
+        
+        if isinstance(direccion, basestring) or isinstance(direccion, NoneType):
+            self.__direccion = direccion
+        else:
+            raise TypeError('Tipo de dato no admitido')
+        
+        if isinstance(telefono, basestring) or isinstance(telefono, NoneType):
+            self.__telefono = telefono
+        else:
+            raise TypeError('Tipo de dato no admitido')  
+        
+        if isinstance(tipo, basestring) or isinstance(tipo, NoneType):
+            self.__tipo = tipo
+        else:
+            raise TypeError('Tipo de dato no admitido')
+        
+        if isinstance(id_juzgado, basestring) or isinstance(id_juzgado, NoneType):
+            self.__id_juzgado = id_juzgado
+        else:
+            raise TypeError('Tipo de dato no admitido')
+            
     #Getters
     def getNombre(self):
         return self.__nombre
@@ -33,17 +56,40 @@ class Juzgado(object):
         return self.__id_juzgado
     #Setters
     def setNombre(self, nombre):
-        self.__nombre = nombre
+        if isinstance(nombre, basestring) or isinstance(nombre, NoneType):
+            self.__nombre = nombre
+        else:
+            raise TypeError('Tipo de dato no admitido')
+        
     def setCiudad(self, ciudad):
-        self.__ciudad = ciudad
+        if isinstance(ciudad, basestring) or isinstance(ciudad, NoneType):
+            self.__ciudad = ciudad
+        else:
+            raise TypeError('Tipo de dato no admitido')
+        
     def setDireccion(self, direccion):
-        self.__direccion = direccion
+        if isinstance(direccion, basestring) or isinstance(direccion, NoneType):
+            self.__direccion = direccion
+        else:
+            raise TypeError('Tipo de dato no admitido')
+        
     def setTelefono(self, telefono):
-        self.__telefono = telefono
+        if isinstance(telefono, basestring) or isinstance(telefono, NoneType):
+            self.__telefono = telefono
+        else:
+            raise TypeError('Tipo de dato no admitido')
+        
     def setTipo(self, tipo):
-        self.__tipo = tipo
+        if isinstance(tipo, basestring) or isinstance(tipo, NoneType):
+            self.__tipo = tipo
+        else:
+            raise TypeError('Tipo de dato no admitido')
+        
     def setId_juzgado(self, id_juzgado):
-        self.__id_juzgado = id_juzgado
+        if isinstance(id_juzgado, basestring) or isinstance(id_juzgado, NoneType):
+            self.__id_juzgado = id_juzgado
+        else:
+            raise TypeError('Tipo de dato no admitido')
     
     def __str__(self):
         return self.getNombre()
