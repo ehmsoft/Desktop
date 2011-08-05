9e49d67a96dcad35eff5bc9318f128c146901097
Andrés Torres <elfotografo007@gmail.com>
2011-08-05 15:34:15 -0500

    Se hace verificacion de tipos en constructor y Setters de Persona

--- AbogadosDesktop/src/core/Persona.py
+++ AbogadosDesktop/src/core/Persona.py
@@ -3,22 +3,56 @@ Created on 04/08/2011
 
 @author: elfotografo007
 '''
-
+from types import IntType, NoneType
 class Persona(object):
     '''
     Clase Persona
+    Tipo 1 Demandante
+    Tipo 2 Demandado
     '''
 
 
-    def __init__(self, tipo, id = None, nombre = None, telefono = None, direccion = None, correo = None, notas = None, id_persona = None):
-        self.__tipo = tipo
-        self.__id = id
-        self.__nombre = nombre
-        self.__telefono = telefono
-        self.__direccion = direccion
-        self.__correo = correo
-        self.__notas = notas
-        self.__id_persona = id_persona
+    def __init__(self, tipo, id = None, nombre = None, telefono = None,
+                direccion = None, correo = None, notas = None, id_persona = None):
+        if isinstance(tipo, IntType):
+            self.__tipo = tipo
+        else:
+            raise TypeError('Tipo de dato no admitido')
+        
+        if isinstance(id, basestring) or isinstance(id, NoneType):
+            self.__id = id
+        else:
+            raise TypeError('Tipo de dato no admitido')   
+        
+        if isinstance(nombre, basestring) or isinstance(nombre, NoneType):
+            self.__nombre = nombre
+        else:
+            raise TypeError('Tipo de dato no admitido')
+        
+        if isinstance(telefono, basestring) or isinstance(telefono, NoneType):
+            self.__telefono = telefono
+        else:
+            raise TypeError('Tipo de dato no admitido')   
+        
+        if isinstance(direccion, basestring) or isinstance(direccion, NoneType):
+            self.__direccion = direccion
+        else:
+            raise TypeError('Tipo de dato no admitido')   
+        
+        if isinstance(correo, basestring) or isinstance(correo, NoneType):
+            self.__correo = correo
+        else:
+            raise TypeError('Tipo de dato no admitido')   
+        
+        if isinstance(notas, basestring) or isinstance(notas, NoneType):
+            self.__notas = notas
+        else:
+            raise TypeError('Tipo de dato no admitido')   
+        
+        if isinstance(id_persona, basestring) or isinstance(id_persona, NoneType):
+            self.__id_persona = id_persona
+        else:
+            raise TypeError('Tipo de dato no admitido')   
     #Getters
     def getTipo(self):
         return self.__tipo
@@ -38,21 +72,52 @@ class Persona(object):
         return self.__id_persona
     #Setters
     def setTipo(self, tipo):
-        self.__tipo = tipo
+        if isinstance(tipo, IntType):
+            self.__tipo = tipo
+        else:
+            raise TypeError('Tipo de dato no admitido')
+        
     def setId(self, id):
-        self.__id = id
+        if isinstance(id, basestring) or isinstance(id, NoneType):
+            self.__id = id
+        else:
+            raise TypeError('Tipo de dato no admitido')
+        
     def setNombre(self, nombre):
-        self.__nombre = nombre
+        if isinstance(nombre, basestring) or isinstance(nombre, NoneType):
+            self.__nombre = nombre
+        else:
+            raise TypeError('Tipo de dato no admitido')
+        
     def setTelefono(self, telefono):
-        self.__telefono = telefono
+        if isinstance(telefono, basestring) or isinstance(telefono, NoneType):
+            self.__telefono = telefono
+        else:
+            raise TypeError('Tipo de dato no admitido')
+        
     def setDireccion(self, direccion):
-        self.__direccion = direccion
+        if isinstance(direccion, basestring) or isinstance(direccion, NoneType):
+            self.__direccion = direccion
+        else:
+            raise TypeError('Tipo de dato no admitido')
+        
     def setCorreo(self, correo):
-        self.__correo = correo
+        if isinstance(correo, basestring) or isinstance(correo, NoneType):
+            self.__correo = correo
+        else:
+            raise TypeError('Tipo de dato no admitido')
+        
     def setNotas(self, notas):
-        self.__notas = notas
+        if isinstance(notas, basestring) or isinstance(notas, NoneType):
+            self.__notas = notas
+        else:
+            raise TypeError('Tipo de dato no admitido')
+        
     def setId_persona(self, id_persona):
-        self.__id_persona = id_persona
+        if isinstance(id_persona, basestring) or isinstance(id_persona, NoneType):
+            self.__id_persona = id_persona
+        else:
+            raise TypeError('Tipo de dato no admitido')
         
     def __str__(self):
         return self.getNombre()
