8dd022b40ddf9b5ae271069589766085cf7898c1
Andrés Torres <elfotografo007@gmail.com>
2011-08-05 15:55:07 -0500

    Se hace verificación de tipos en Categoria

--- AbogadosDesktop/src/core/Categoria.py
+++ AbogadosDesktop/src/core/Categoria.py
@@ -3,7 +3,7 @@ Created on 03/08/2011
 
 @author: elfotografo007
 '''
-
+from types import NoneType
 class Categoria(object):
     '''
     Clase Categoria
@@ -11,15 +11,35 @@ class Categoria(object):
 
 
     def __init__(self, descripcion, id_categoria = None):
-        self.__id_categoria = id_categoria
-        self.__descripcion = descripcion
+        if isinstance(id_categoria, basestring) or isinstance(id_categoria, NoneType):
+            self.__id_categoria = id_categoria
+        else:
+            raise TypeError('Tipo de dato no admitido')
+        
+        if isinstance(descripcion, basestring):
+            self.__descripcion = descripcion
+        else:
+            raise TypeError('Tipo de dato no admitido')
+    
+    #Getters
     def getId_categoria(self):
         return self.__id_categoria
     def getDescripcion(self):
         return self.__descripcion
+    
+    #Setters
     def setId_categoria(self, id_categoria):
-        self.__id_categoria = id_categoria
+        if isinstance(id_categoria, basestring) or isinstance(id_categoria, NoneType):
+            self.__id_categoria = id_categoria
+        else:
+            raise TypeError('Tipo de dato no admitido')
+        
     def setDescripcion(self, descripcion):
-        self.__descripcion = descripcion
+        if isinstance(descripcion, basestring):
+            self.__descripcion = descripcion
+        else:
+            raise TypeError('Tipo de dato no admitido')
+        
+        
     def __str__(self):
         return self.getDescripcion()
