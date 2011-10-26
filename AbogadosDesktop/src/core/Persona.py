e1236228812befb6e693d96068d09be435908801
Andrés Torres <elfotografo007@gmail.com>
2011-10-26 15:06:11 -0500

    Se agrega soporte de campos personalizados a Persona

--- AbogadosDesktop/src/core/Persona.py
+++ AbogadosDesktop/src/core/Persona.py
@@ -3,7 +3,8 @@ Created on 04/08/2011
 
 @author: elfotografo007
 '''
-from types import IntType, NoneType
+from types import IntType, NoneType, ListType
+from CampoPersonalizado import CampoPersonalizado
 
 class Persona(object):
     '''
@@ -14,7 +15,7 @@ class Persona(object):
 
 
     def __init__(self, tipo, id = None, nombre = None, telefono = None,
-                direccion = None, correo = None, notas = None, id_persona = None):
+                direccion = None, correo = None, notas = None, id_persona = None, campos=[]):
         if isinstance(tipo, IntType):
             self.__tipo = tipo
         else:
@@ -53,7 +54,20 @@ class Persona(object):
         if isinstance(id_persona, basestring) or isinstance(id_persona, NoneType):
             self.__id_persona = id_persona
         else:
-            raise TypeError('Tipo de dato no admitido')   
+            raise TypeError('Tipo de dato no admitido') 
+        if isinstance(campos, ListType):
+            self.__campos = campos
+        else:
+            raise TypeError('Tipo de dato no admitido')  
+    
+    def addCampo(self, campo):
+        if isinstance(campo, CampoPersonalizado):
+            self.__campos.append(campo)
+        else:
+            raise TypeError('Tipo de dato no admitido')
+        
+    def delCampo(self, campo):
+        self.__campos.remove(campo)
     #Getters
     def getTipo(self):
         return self.__tipo
@@ -71,6 +85,8 @@ class Persona(object):
         return self.__notas
     def getId_persona(self):
         return self.__id_persona
+    def getCampos(self):
+        return self.__campos
     #Setters
     def setTipo(self, tipo):
         if isinstance(tipo, IntType):
@@ -119,6 +135,11 @@ class Persona(object):
             self.__id_persona = id_persona
         else:
             raise TypeError('Tipo de dato no admitido')
-        
+    def setCampos(self, campos):
+        if isinstance(campos, ListType):
+            self.__campos = campos
+        else:
+            raise TypeError('Tipo de dato no admitido')
+    
     def __str__(self):
         return self.getNombre()
