997388d565b3494c3763760a01d5a52483c84ee1
Andrés Torres <elfotografo007@gmail.com>
2011-11-09 15:40:00 -0500

    Se crea plantilla de SyncManager

---
+++ AbogadosDesktop/src/persistence/SyncManager.py
@@ -0,0 +1,39 @@
+'''
+Created on 08/11/2011
+
+@author: elfotografo007
+'''
+from ConnectionManager import ConnectionManager
+import sqlite3
+from datetime import datetime
+
+class SyncManager(object):
+    '''
+    Clase SyncManager
+    '''
+    
+
+    def __init__(self):
+        self.__conMgr = ConnectionManager()
+        
+    def sincronizar(self, archivo_movil):
+        try:
+            self.__conMgr.prepararBD()
+            connLocal = sqlite3.connect(self.__conMgr.getDbLocation(), detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+            connLocal.row_factory = sqlite3.Row
+            connMovil = sqlite3.connect(self.__conMgr.getDbLocation(), detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+            connMovil.row_factory = sqlite3.Row
+            cLocal = connLocal.cursor()
+            cMovil = connMovil.cursor()
+            
+            
+            connMovil.commit()
+            connLocal.commit()            
+        except Exception as e:
+            raise e
+        finally:
+            connLocal.close()
+            connMovil.close()
+       
+        
+        
