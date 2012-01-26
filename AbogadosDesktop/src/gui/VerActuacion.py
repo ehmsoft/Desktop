14c35925bcfdb87c2ff9bf1bbeaa76e73ffe24de
Andrés Torres <elfotografo007@gmail.com>
2012-01-26 08:23:29 -0500

    Se implementa VerActuacion

---
+++ AbogadosDesktop/src/gui/VerActuacion.py
@@ -0,0 +1,26 @@
+'''
+Created on 26/01/2012
+
+@author: elfotografo007
+'''
+from PySide.QtGui import *
+from PySide.QtCore import *
+from gui.VerActuacionScreen import Ui_VerActuacion
+
+
+class VerActuacion(QWidget, Ui_VerActuacion):
+    def __init__(self, actuacion = None, parent = None):
+        super(VerActuacion, self).__init__(parent)
+        self.__actuacion = actuacion
+        self.setupUi(self)
+        if self.__actuacion:
+            self.lblJuzgado.setText(str(self.__actuacion.getJuzgado()))
+            self.lblDescripcion.setText(self.__actuacion.getDescripcion())
+            self.dteFecha.setDateTime(self.__actuacion.getFecha())
+            self.dteFechaProxima.setDateTime(self.__actuacion.getFechaProxima())
+            for campo in self.__actuacion.getCampos():
+                label = QLabel()
+                label.setText('%s:' % campo.getNombre())
+                lblBox = QLabel()
+                lblBox.setText(campo.getValor())
+                self.formLayout.addRow(label,lblBox)
