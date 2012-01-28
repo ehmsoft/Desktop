1b74b7bab7782332cde30bd6c39dd6db96be0ed9
Andrés Torres <elfotografo007@gmail.com>
2012-01-28 10:39:28 -0500

    Se implementa VerPlantilla

---
+++ AbogadosDesktop/src/gui/VerPlantilla.py
@@ -0,0 +1,34 @@
+'''
+Created on 28/01/2012
+
+@author: elfotografo007
+'''
+from PySide.QtGui import *
+from PySide.QtCore import *
+from gui.VerPlantillaScreen import Ui_VerPlantilla
+
+class VerPlantilla(QWidget, Ui_VerPlantilla):
+    def __init__(self, plantilla = None, parent = None):
+        super(VerPlantilla, self).__init__(parent)
+        self.__plantilla = plantilla
+        self.setupUi(self)
+    
+        if self.__plantilla:
+            self.lblNombre.setText(str(self.__plantilla))
+            self.lblDemandante.setText(str(self.__plantilla.getDemandante()))
+            self.lblDemandado.setText(str(self.__plantilla.getDemandado()))
+            self.lblJuzgado.setText(str(self.__plantilla.getJuzgado()))
+            self.lblRadicado.setText(self.__plantilla.getRadicado())
+            self.lblRadicadoUnico.setText(self.__plantilla.getRadicadoUnico())
+            self.lblEstado.setText(self.__plantilla.getEstado())
+            self.lblCategoria.setText(str(self.__plantilla.getCategoria()))
+            self.lblTipo.setText(str(self.__plantilla.getTipo()))
+            self.lblPrioridad.setNum(self.__plantilla.getPrioridad())
+            self.lblNotas.setText(self.__plantilla.getNotas())
+            
+            for campo in self.__plantilla.getCampos():
+                label = QLabel()
+                label.setText('%s:' % campo.getNombre())
+                lblBox = QLabel()
+                lblBox.setText(campo.getValor())
+                self.formLayout.addRow(label,lblBox)
