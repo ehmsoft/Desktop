908e71e027413d5b12e7172119f12a739d942ffe
Andrés Torres <elfotografo007@gmail.com>
2012-02-01 10:35:26 -0500

    Se agregan getter y setter a ColumnaWidget

--- AbogadosDesktop/src/gui/Columna.py
+++ AbogadosDesktop/src/gui/Columna.py
@@ -10,17 +10,23 @@ from PySide.QtCore import *
 class ColumnaWidget(QWidget):
     def __init__(self, centralWidget,parent=None):
         super(ColumnaWidget, self).__init__(parent)
-        self.centralWidget = centralWidget
+        self.__centralWidget = centralWidget
         layout = QVBoxLayout()
         btnLayout = QHBoxLayout()
         btnLayout.addStretch()
         self.btnAgregar = QPushButton('+')
         btnLayout.addWidget(self.btnAgregar)
-        layout.addWidget(self.centralWidget)
+        layout.addWidget(self.__centralWidget)
         layout.addStretch()
         layout.addLayout(btnLayout)
         self.setLayout(layout)
         self.connect(self.btnAgregar, SIGNAL('clicked()'), self.click)
+        
+    def setCentralWidget(self, widget):
+        self.__centralWidget = widget
+    
+    def getCentralWidget(self):
+        return self.__centralWidget
         
     def click(self):
         self.emit(SIGNAL('clicked()')) 
