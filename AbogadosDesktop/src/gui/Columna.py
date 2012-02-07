8cfa54869fc1104bf2f0b01319503ef82febd0b9
Andrés Torres <elfotografo007@gmail.com>
2012-02-07 14:13:57 -0500

    Se crea la clase ColumnaDerecha

--- AbogadosDesktop/src/gui/Columna.py
+++ AbogadosDesktop/src/gui/Columna.py
@@ -30,3 +30,36 @@ class ColumnaWidget(QWidget):
         
     def click(self):
         self.emit(SIGNAL('clicked()')) 
+        
+class ColumnaDerecha(QWidget):
+    def __init__(self, titulo,centralWidget,parent=None):
+        super(ColumnaWidget, self).__init__(parent)
+        self.__centralWidget = centralWidget
+        self.__titulo = titulo
+        layout = QVBoxLayout()
+        btnLayout = QHBoxLayout()
+        btnLayout.addStretch()
+        self.btnEditar = QPushButton('Editar')
+        self.btnEliminar = QPushButton('Eliminar')
+        btnLayout.addWidget(self.btnEditar)
+        btnLayout.addWidget(self.btnEliminar)
+        if self.__titulo:
+            layout.addWidget(QLabel(self.__centralWidget.windowTitle()))
+        layout.addWidget(self.__centralWidget)
+        layout.addStretch()
+        layout.addLayout(btnLayout)
+        self.setLayout(layout)
+        self.connect(self.btnEditar, SIGNAL('clicked()'), self.editarClicked)
+        self.connect(self.btnEliminar, SIGNAL('clicked()'), self.eliminarClicked)
+    
+    def setCentralWidget(self, widget):
+        self.__centralWidget = widget
+                
+    def getCentralWidget(self):
+        return self.__centralWidget
+        
+    def editarClicked(self):
+        self.emit(SIGNAL('editarClicked()')) 
+        
+    def eliminarClicked(self):
+        self.emit(SIGNAL('eliminarClicked()')) 
