6cffb53306e48383d214646e11ddeb4f29cd8c53
sancospi <sancospi@gmail.com>
2012-02-24 22:14:56 -0500

    Se define archivo para funciones centralizadas

---
+++ AbogadosDesktop/src/gui/Util.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from PySide import QtCore, QtGui
+
+def reject(dialog, dirty):
+    if dirty:
+        message = QtGui.QMessageBox()
+        message.setIcon(QtGui.QMessageBox.Question)
+        message.setStandardButtons(QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
+        message.setDefaultButton(QtGui.QMessageBox.No)
+        message.setText(unicode("¿Desea descartar los cambios?"))
+        ret = message.exec_()
+        if ret == QtGui.QMessageBox.Yes:
+            return QtGui.QDialog.reject(dialog)
+    else:
+        return QtGui.QDialog.reject(dialog)
