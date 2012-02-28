55f4a2eba902c881adb672b243c8bf55146a4dac
sancospi <sancospi@gmail.com>
2012-02-27 21:22:29 -0500

    Se realiza implementación de la búsqueda para todo tipo de lista

--- AbogadosDesktop/src/gui/ListadoBusqueda.py
+++ AbogadosDesktop/src/gui/ListadoBusqueda.py
@@ -1,57 +1,137 @@
+# -*- coding: utf-8 -*-
 '''
 Created on 27/02/2012
 
 @author: harold
 '''
-from PySide.QtCore import *
-from PySide.QtGui import *
-import sys
+from PySide import QtGui
 from Listado import Listado
-from persistence.Persistence import Persistence
 from core.Proceso import Proceso
+from core.Plantilla import Plantilla
+from core.Persona import Persona
+from core.Juzgado import Juzgado
+from core.Actuacion import Actuacion
+from core.Categoria import Categoria
+from core.CampoPersonalizado import CampoPersonalizado
 
-class ListadoBusqueda(QDialog):
+class ListadoBusqueda(Listado):
     '''
     classdocs
     '''
 
-    def __init__(self):
+    def __init__(self, listado = [], parent = None):
         '''
         Constructor
         '''
-        self.lista = QListWidget(self)
-        self.txtBuscar = QLineEdit(self)
-        self.listaOriginal = Persistence().consultarProcesos()
+        super(ListadoBusqueda, self).__init__(listado, parent)
+        
+        self.listaOriginal = listado
         self.listadoActual = self.listaOriginal
-        self.lista.addItems(self.listaOriginal)
+        self.buscar = CampoBusqueda()
+        self.buscar.show()
+                
         self.texto = None
-        self.txtBuscar.textChanged.connect(self.changed)
+        self.buscar.getTextEdit().textChanged.connect(self.__changed)
+        self.buscar.getButton().clicked.connect(self.__click)
         
-    def filtro(self, elemento):
-        demandante = elemento.getDemandante().getNombre().lower()
-        demandado = elemento.getDemandado().getNombre().lower()
-        juzgado = elemento.getJuzgado().getNombre().lower()
-        radicado = elemento.getRadicado().lower()
-        radicadoUnido = elemento.getRadicadoUnico().lower()
-        categoria = elemento.getCategoria().getNombre().lower()
-        notas = elemento.getNotas().lower()
-        
-        keywords = [demandante,demandado,juzgado,radicado,radicadoUnido,categoria,notas]
-        self.texto.lower()
-        for key in keywords:
-            if self.texto in key:
+    def __filtro(self, elemento):
+        for key in self.__getKeyWords(elemento):
+            if key is not None and self.texto in key.lower():
                 return True
             
         return False
+    
+    def __getKeyWords(self, elemento):
+        if isinstance(elemento, Proceso):
+            demandante = elemento.getDemandante().getNombre()
+            demandado = elemento.getDemandado().getNombre()
+            juzgado = elemento.getJuzgado().getNombre()
+            radicado = elemento.getRadicado()
+            radicadoUnido = elemento.getRadicadoUnico()
+            categoria = elemento.getCategoria().getDescripcion()
+            notas = elemento.getNotas()
+            
+            return [demandante, demandado, juzgado, radicado, radicadoUnido, categoria, notas]
+        elif isinstance(elemento, Plantilla):
+            demandante = elemento.getDemandante().getNombre()
+            demandado = elemento.getDemandado().getNombre()
+            juzgado = elemento.getJuzgado().getNombre()
+            radicado = elemento.getRadicado()
+            radicadoUnido = elemento.getRadicadoUnico()
+            categoria = elemento.getCategoria().getDescripcion()
+            notas = elemento.getNotas()
+            
+            return [demandante, demandado, juzgado, radicado, radicadoUnido, categoria, notas]
+        elif isinstance(elemento, Persona):
+            nombre = elemento.getNombre()
+            telefono = elemento.getTelefono()
+            cedula = elemento.getId()
+            
+            return [nombre, telefono, cedula]
+        elif isinstance(elemento, Juzgado):
+            nombre = elemento.getNombre()
+            telefono = elemento.getTelefono()
+            ciudad = elemento.getCiudad()
+            tipo = elemento.getTipo()
+            
+            return [nombre, telefono, ciudad, tipo]
+        elif isinstance(elemento, Actuacion):
+            descripcion = elemento.getDescripcion()
+            date = elemento.getFechaProxima().date()
+            
+            fechaProxima1 = '{:%d %m %Y}'.format(date)
+            fechaProxima2 = '{:%d%m%Y}'.format(date)
+            fechaProxima3 = '{:%d-%m-%Y}'.format(date)
+            fechaProxima4 = '{:%d/%m/%Y}'.format(date)
+            
+            juzgado = elemento.getJuzgado().getNombre()
+            
+            return [descripcion, fechaProxima1, fechaProxima2, fechaProxima3, fechaProxima4, juzgado]
+        elif isinstance(elemento, Categoria):
+            return [elemento.getDescripcion()]
+        elif isinstance(elemento, CampoPersonalizado):
+            return [elemento.getNombre()]
+        else: 
+            return [unicode(elemento)]            
+                
+    def __changed(self, text):
+        self.texto = text.lower()
+        self.listadoActual = filter(self.__filtro, self.listaOriginal)
+        self.__montarLista()
         
-    def changed(self):
-        self.texto = self.txtBuscar.text()
-        self.listadoActual = filter(self.filtro, self.listaOriginal)
-        self.montarLista()
+    def __click(self):
+        texto = self.buscar.getTextEdit().text()
+        self.__changed(texto)
         
-    def montarLista(self):
-        for x in range(0,len(self.listadoActual)):
-            self.lista.itemAt(0)
-        self.lista.addItems(self.listadoActual)
+    def getSearchField(self):
+        self.buscar.show()
+        return self.buscar
         
-app = QApplication(sys.argv)
+    def __montarLista(self):
+        while self.count() > 0:
+            self.takeItem(0)
+        self.addItems(self.listadoActual)
+    
+    def showSearchField(self):
+        self.buscar.show()
+        
+class CampoBusqueda(QtGui.QWidget):
+    def __init__(self, parent = None):
+        '''
+        Constructor
+        '''
+        super(CampoBusqueda, self).__init__(parent)
+        iconoBuscar = QtGui.QIcon('./images/iconoBuscar.png')
+        buscar = QtGui.QHBoxLayout(self)
+        self.txtBuscar = QtGui.QLineEdit(self)
+        self.btnBuscar = QtGui.QPushButton(self)
+        self.btnBuscar.setFlat(True)
+        self.btnBuscar.setIcon(iconoBuscar)
+        buscar.addWidget(self.txtBuscar)
+        buscar.addWidget(self.btnBuscar)
+        
+    def getTextEdit(self):
+        return self.txtBuscar
+    
+    def getButton(self):
+        return self.btnBuscar
