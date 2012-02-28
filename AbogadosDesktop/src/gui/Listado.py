a520df3dbaf0f3e77348645147d5289a172ccc5c
sancospi <sancospi@gmail.com>
2012-02-27 21:19:49 -0500

    Cambios no funcionales para futura compatibilidad

--- AbogadosDesktop/src/gui/Listado.py
+++ AbogadosDesktop/src/gui/Listado.py
@@ -1,10 +1,10 @@
+# -*- coding: utf-8 -*-
 '''
 Created on 19/01/2012
 
 @author: esteban
 '''
-from PySide.QtCore import *
-from PySide.QtGui import *
+from PySide import QtGui
 from ItemListas import ItemListas
 from types import ListType
 from core.Persona import Persona
@@ -12,62 +12,62 @@ from core.Juzgado import Juzgado
 from persistence.Persistence import Persistence
 
 
-class Listado(QListWidget):
-    def __init__(self,listaObjetos = [],parent = None):
-        super(Listado,self).__init__(parent)
+class Listado(QtGui.QListWidget):
+    def __init__(self, listaObjetos = [], parent = None):
+        super(Listado, self).__init__(parent)
         
     
         if listaObjetos is None or len(listaObjetos) is 0:
-                listaObjetos = [] 
+            listaObjetos = [] 
               
-        elif isinstance(listaObjetos[0], Persona):
-            
+        elif isinstance(listaObjetos[0], Persona):            
             try:
-                p= Persistence()
+                p = Persistence()
                 vacio = p.consultarPersona("1", listaObjetos[0].getTipo())                
                 listaObjetos.remove(vacio)
-            except Exception,e:
+            except Exception, e:
                 print e
         elif isinstance(listaObjetos[0], Juzgado):
             try:
-                p= Persistence()
+                p = Persistence()
                 vacio = p.consultarJuzgado("1")                
                 listaObjetos.remove(vacio)
-            except Exception,e:
+            except Exception, e:
                 print e
         
         self.setMouseTracking(True)       
-        if isinstance(listaObjetos, ListType):
-            self.__listaObjetos = listaObjetos
-                                      
-            for objeto in listaObjetos:
+        self.addItems(listaObjetos)
+        
+    def addItems(self, items):
+        if isinstance(items, ListType):                                      
+            for objeto in items:
                 item = ItemListas(objeto)
-                item.setToolTip(unicode(objeto))
-                item.setStatusTip(unicode(objeto))
+                #item.setToolTip(unicode(objeto))
+                #item.setStatusTip(unicode(objeto))
                 self.addItem(item)
         else:
             raise TypeError('Tipo de dato no admitido')
         
-    def add(self,objeto):
+    def add(self, objeto):
         item = ItemListas(objeto)
-        item.setToolTip(unicode(objeto))
-        item.setStatusTip(unicode(objeto))
+        #item.setToolTip(unicode(objeto))
+        #item.setStatusTip(unicode(objeto))
         self.addItem(item)
         
     def remove(self):        
         objeto = self.currentItem()
-        if QMessageBox.question(self,"Eliminar","Desea eliminar "+objeto.text()+" ?",QMessageBox.Yes|QMessageBox.No) == QMessageBox.Yes:
+        pregunta = QtGui.QMessageBox.question(self, "Eliminar", "¿Desea eliminar %s?" % objeto.text(), QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
+        pregunta.setDefaultButton(QtGui.QMessageBox.No)
+        ret = pregunta.exec_()
+        if ret == QtGui.QMessageBox.Yes:
             self.takeItem(self.currentRow())
             return True
         else:
             return False
             
-    def replace(self,new):
+    def replace(self, new):
         item = self.currentItem()
         item.setObjeto(new)
         
     def getSelectedItem(self):
         return self.currentItem().getObjeto()
-        
-
-
