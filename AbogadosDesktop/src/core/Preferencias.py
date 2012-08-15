967cda86d7aeee57b489e0531f29b0c5a43631c6
Andrés Torres <elfotografo007@gmail.com>
2012-08-15 12:19:12 -0500

    Se retiran preferencias no usadas Se agregan constantes para las preferencias y se usan en algunos mètodos

--- AbogadosDesktop/src/core/Preferencias.py
+++ AbogadosDesktop/src/core/Preferencias.py
@@ -4,7 +4,6 @@ Created on 29/03/2012
 
 @author: esteban
 '''
-from types import  ListType
 from types import IntType
 from persistence.Persistence import Persistence
 
@@ -26,47 +25,45 @@ class Preferencias(object):
     TXTEVENTOS = u'Eventos Próximos'
     CANTEVENTOS = 10
     
+    CORREO = 10402
+    CANTIDAD_EVENTOS = 10501
+    TIPO_ALARMA = 10601
+    LLAVE = 998
+    VERSION = 999
+    ULTIMA_SINC = 997
+    
     def __init__(self):
         self.p = Persistence()
         preferencias = self.p.consultarPreferencias()
         for llaveD, valor in preferencias.iteritems():
-            if llaveD == 10101:
-                self.__listaMainApp = valor
             #consultar Preferencias:  correo 
-            elif llaveD == 10402:
+            if llaveD == self.CORREO:
                 self.__correo = valor
             #consultar Preferencias: Cantidad Eventos Proximos 
-            elif llaveD == 10501:
+            elif llaveD == self.CANTIDAD_EVENTOS:
                 self.__cantidadEventos = valor
-            #consultar Preferencias: Tipo Alarma 0 ninguni, 1 correo y alerta, 2 solo correo, 3 solo alerta
-            elif llaveD == 10601:
+            #consultar Preferencias: Tipo Alarma es un valor binario, primer bit mensaje emergente, segundo bit ,emsaje en icono de notificación, tercer bit correo electrónico
+            elif llaveD == self.TIPO_ALARMA:
                 self.__tipoAlarma = valor    
-            #consultar Preferencias: Cantidad Maxima Copias de Seguridad 
-            elif llaveD == 10701:
-                self.__cantCopiaSeg = valor
             #consultar Preferencias: llave 
-            elif llaveD == 998:
+            elif llaveD == self.LLAVE:
                 self.__llave = valor
             #consultar Preferencias: Version 
-            elif llaveD == 999:
+            elif llaveD == self.VERSION:
                 self.__version = valor
             #consultar Preferencias: Ultima sincronizacion 
-            elif llaveD == 997:
+            elif llaveD == self.ULTIMA_SINC:
                 self.__ultimaSinc = valor
     
     #Getters    
     def getId_preferencia(self):
         return self.__id_preferencia
-    def getListaMainApp(self):
-        return self.__listaMainApp
     def getCantidadEventos(self):
         return self.__cantidadEventos
     def getCorreo(self):
         return self.__correo
     def getTipoAlarma(self):
         return self.__tipoAlarma
-    def getCantCopiaSeg(self):
-        return self.__cantCopiaSeg
     def getLlave(self):
         return self.__llave
     def getVersion(self):
@@ -75,12 +72,6 @@ class Preferencias(object):
         return self.__ultimaSinc
     
     #Setters
-    def setListaMainApp(self, listaMainApp):
-        if isinstance(listaMainApp, ListType):
-            self.__listaMainApp = listaMainApp
-            self.p.actualizarPreferencia(id=10101 , valor=listaMainApp)
-        else:
-            raise TypeError('Tipo de dato no admitido')
     def setCantidadEventos(self, cantidadEventos):
         if isinstance(cantidadEventos, IntType):
             self.__cantidadEventos = cantidadEventos
@@ -97,12 +88,6 @@ class Preferencias(object):
         if isinstance(tipoAlarma, IntType):
             self.__tipoAlarma = tipoAlarma
             self.p.actualizarPreferencia(id=10601, valor=tipoAlarma)
-        else:
-            raise TypeError('Tipo de dato no admitido')
-    def setCantCopiaSeg(self, cantCopiaSeg):
-        if isinstance(cantCopiaSeg, IntType):
-            self.__cantCopiaSeg = cantCopiaSeg
-            self.p.actualizarPreferencia(id=10701, valor=cantCopiaSeg)
         else:
             raise TypeError('Tipo de dato no admitido')
     def setLlave(self, llave):
@@ -127,12 +112,9 @@ class Preferencias(object):
                              
     def borrarPreferencia(self, id_preferencia):
         p = Persistence()
-        if id_preferencia == 10101:
-            self.__listaMainApp = '20111,20105,20115,20114,20124,20123,20101,20107,20102,20108,20109'
-            p.borrarPreferencia(id_preferencia=10101) 
         #consultar Preferencias:  correo 
-        elif id_preferencia == 10402:
-            self.__correo = ' '
+        if id_preferencia == 10402:
+            self.__correo = ''
             p.borrarPreferencia(id_preferencia=10402)               
         #consultar Preferencias: Cantidad Eventos Proximos 
         elif id_preferencia == 10501:
@@ -140,12 +122,8 @@ class Preferencias(object):
             p.borrarPreferencia(id_preferencia=10501) 
         #consultar Preferencias: Tipo Alarma 0 ninguni, 1 correo y alerta, 2 solo correo, 3 solo alerta
         elif id_preferencia == 10601:
-            self.__tipoAlarma = 1
+            self.__tipoAlarma = 0
             p.borrarPreferencia(id_preferencia=10601)     
-        #consultar Preferencias: Cantidad Maxima Copias de Seguridad 
-        elif id_preferencia == 10701:
-            self.__cantCopiaSeg = 5
-            p.borrarPreferencia(id_preferencia=10701) 
         #consultar Preferencias: llave 
         elif id_preferencia == 998:
             self.__llave = 0000
@@ -159,48 +137,39 @@ class Preferencias(object):
             self.__ultimaSinc = 0000
             p.borrarPreferencia(id_preferencia=997) 
             
-def borrarPreferencias(self):
-        p = Persistence()
-        p.borrarPreferencias()
-        self.__listaMainApp = '20111,20105,20115,20114,20124,20123,20101,20107,20102,20108,20109'
-        self.__correo = ''
-        self.__cantidadEventos = 10
-        self.__tipoAlarma = 1
-        self.__cantCopiaSeg = 5
-        self.__llave = 0000
-        self.__version = 1
-        self.__ultimaSinc = 0000
-        
-def actualizarPrefrencia(self, id_preferencia, valor):
-        p = Persistence()
-        if id_preferencia == 10101:
-            p.actualizarPreferencia(id=id_preferencia, valor=valor)
-            self.__listaMainApp = valor
-        #consultar Preferencias:  correo 
-        elif id_preferencia == 10402:
-            p.actualizarPreferencia(id=id_preferencia, valor=valor)
-            self.__correo = valor              
-        #consultar Preferencias: Cantidad Eventos Proximos 
-        elif id_preferencia == 10501:
-            p.actualizarPreferencia(id=id_preferencia, valor=valor)
-            self.__cantidadEventos = valor
-        #consultar Preferencias: Tipo Alarma 0 ninguni, 1 correo y alerta, 2 solo correo, 3 solo alerta
-        elif id_preferencia == 10601:
-            p.actualizarPreferencia(id=id_preferencia, valor=valor)
-            self.__tipoAlarma = valor
-        #consultar Preferencias: Cantidad Maxima Copias de Seguridad 
-        elif id_preferencia == 10701:
-            p.actualizarPreferencia(id=id_preferencia, valor=valor)
-            self.__cantCopiaSeg = valor
-        #consultar Preferencias: llave 
-        elif id_preferencia == 998:
-            p.actualizarPreferencia(id=id_preferencia, valor=valor)
-            self.__llave = valor
-        #consultar Preferencias: Version 
-        elif id_preferencia == 999:
-            p.actualizarPreferencia(id=id_preferencia, valor=valor)
-            self.__version = valor
-        #consultar Preferencias: Ultima sincronizacion 
-        elif id_preferencia == 997:
-            p.actualizarPreferencia(id=id_preferencia, valor=valor)
-            self.__ultimaSinc = 0000
+    def borrarPreferencias(self):
+            p = Persistence()
+            p.borrarPreferencias()
+            self.__correo = ''
+            self.__cantidadEventos = 10
+            self.__tipoAlarma = 1
+            self.__llave = 0
+            self.__version = 1
+            self.__ultimaSinc = 0
+            
+    def actualizarPrefrencia(self, id_preferencia, valor):
+            p = Persistence()
+            #consultar Preferencias:  correo 
+            if id_preferencia == 10402:
+                p.actualizarPreferencia(id=id_preferencia, valor=valor)
+                self.__correo = valor              
+            #consultar Preferencias: Cantidad Eventos Proximos 
+            elif id_preferencia == 10501:
+                p.actualizarPreferencia(id=id_preferencia, valor=valor)
+                self.__cantidadEventos = valor
+            #consultar Preferencias: Tipo Alarma 0 ninguni, 1 correo y alerta, 2 solo correo, 3 solo alerta
+            elif id_preferencia == 10601:
+                p.actualizarPreferencia(id=id_preferencia, valor=valor)
+                self.__tipoAlarma = valor
+            #consultar Preferencias: llave 
+            elif id_preferencia == 998:
+                p.actualizarPreferencia(id=id_preferencia, valor=valor)
+                self.__llave = valor
+            #consultar Preferencias: Version 
+            elif id_preferencia == 999:
+                p.actualizarPreferencia(id=id_preferencia, valor=valor)
+                self.__version = valor
+            #consultar Preferencias: Ultima sincronizacion 
+            elif id_preferencia == 997:
+                p.actualizarPreferencia(id=id_preferencia, valor=valor)
+                self.__ultimaSinc = 0
