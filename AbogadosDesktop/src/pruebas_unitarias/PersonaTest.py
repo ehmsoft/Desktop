7d56c1e9a583791be3ab80a9fd9acef5d10fb5c0
Andrés Torres <elfotografo007@gmail.com>
2012-12-05 15:09:05 -0500

    Se crea clase de pruebas unitarias PersonaTest

---
+++ AbogadosDesktop/src/pruebas_unitarias/PersonaTest.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 05/12/2012
+
+@author: elfotografo007
+'''
+import unittest
+from core.Persona import Persona
+from core.Proceso import Proceso
+from core.Actuacion import Actuacion
+from core.CampoPersonalizado import CampoPersonalizado
+from core.Categoria import Categoria
+from core.Juzgado import Juzgado
+from core.Plantilla import Plantilla
+from core.CitaCalendario import CitaCalendario
+from core.ActuacionCritica import ActuacionCritica
+
+class CoreTest(unittest.TestCase):
+    '''
+    Clase para correr pruebas unitarias sobre el paquete core
+    '''
+    def setUp(self):
+        self.demandante = Persona(1, "1088279267", "Andres", "3168746839", "Calle", "elfotografo007@gmail.com", "Esto es una nota")
+        self.demandado = Persona(2, "1088286673", "Sara", "3117607783", "Barrio", "saris021@hotmail.com", "Esto es una nota")
+        
+    def testPersonasDiferentes(self):
+        self.assertNotEqual(self.demandante, self.demandado, "El demandante y el demandado deben ser diferentes")
+        
+    def testGetNombre(self):
+        self.assertEqual(self.demandante.getNombre(), "Andres", "El nombre del demandante debe ser Andres")
+        self.assertEqual(self.demandado.getNombre(), "Sara", "El nombre del demandado deber ser Sara")
+        
+    def testGetId(self):
+        self.assertEqual(self.demandante.getId(), "1088279267", u"La cédula del demandante debe ser 1088279267")
+        self.assertEqual(self.demandado.getId(), "1088286673", u"La cédula del demandado deber ser 1088286673")
+        
+    def testGetTelefono(self):
+        self.assertEqual(self.demandante.getTelefono(), "3168746839", u"El teléfono del demandante debe ser 3168746839")
+        self.assertEqual(self.demandado.getTelefono(), "3117607783", "El teléfono del demandado deber ser 3117607783")
+    
+    def testGetDireccion(self):
+        self.assertEqual(self.demandante.getDireccion(), "Calle", u"La dirección del demandante debe ser Calle")
+        self.assertEqual(self.demandado.getDireccion(), "Barrio", u"La dirección del demandado deber ser Barrio")
+        
+    def testGetCorreos(self):
+        self.assertEqual(self.demandante.getCorreo(), "elfotografo007@gmail.com", u"El correo del demandante debe ser elfotografo007@gmail.com")
+        self.assertEqual(self.demandado.getCorreo(), "saris021@hotmail.com", u"El correo del demandado deber ser saris021@hotmail.com")
+        
+    def testGetNotas(self):
+        self.assertEqual(self.demandante.getNotas(), "Esto es una nota", u"Las notas del demandante debe ser Esto es una nota")
+        self.assertEqual(self.demandado.getNotas(), "Esto es una nota", u"Las notas del demandado deber ser Esto es una nota")
+    
+    def testGetId_persona(self):
+        self.assertIsNone(self.demandante.getId_persona(), "El id_persona del demandante debe ser None")
+        self.assertIsNone(self.demandado.getId_persona(), "El id_persona del demandado debe ser None")
+        
+    def testAgregarCampo(self):
+        campo = CampoPersonalizado("cumpleanos", "10")
+        self.demandante.addCampo(campo)
+        self.demandado.addCampo(campo)
+        self.assertListEqual(self.demandante.getCampos(), self.demandado.getCampos(), "Se debe devolver una lista con el campo personalizado cumpleanos")
+        
+    def testSetCampos(self):
+        self.assertRaises(TypeError, self.demandante.setCampos, None)
+        self.assertRaises(TypeError, self.demandado.setCampos, None)
+        
+if __name__ == "__main__":
+    unittest.main()
